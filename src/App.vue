3ae2b560a9d5dd60a2c7e7772950505760ee495b
Braden <bmw02002turbo@gmail.com>
2021-01-17 23:53:12 -0800

    Added prettier and settings button

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,39 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <v-row>
-          <v-col sm="6" offset-sm="3">
-            <Pomodoro />
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-row>
+                    <v-col sm="6" offset-sm="3">
+                        <Pomodoro />
+                    </v-col>
+
+                    <v-btn color="secondary" dark small absolute top right fab>
+                        <v-icon>mdi-cog-outline</v-icon>
+                    </v-btn>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import Pomodoro from "./components/Pomodoro.vue"
+import Pomodoro from "./components/Pomodoro.vue";
 
 export default {
-  name: "App",
-  components: {
-    Pomodoro,
-  },
-}
+    name: "App",
+    components: {
+        Pomodoro
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,107 +1,114 @@
 <template>
-  <v-card class="mt-8">
-    <v-tabs @change="changeCurrentTimer" v-model="currentTimer" grow>
-      <v-tab v-for="timer in timers" :key="timer.name">
-        {{ timer.name }}
-      </v-tab>
-      <v-tabs-items v-model="currentTimer">
-        <v-tab-item> </v-tab-item>
-      </v-tabs-items>
-    </v-tabs>
+    <v-card class="mt-8">
+        <v-tabs @change="changeCurrentTimer" v-model="currentTimer" grow>
+            <v-tab v-for="timer in timers" :key="timer.name">
+                {{ timer.name }}
+            </v-tab>
+            <v-tabs-items v-model="currentTimer">
+                <v-tab-item> </v-tab-item>
+            </v-tabs-items>
+        </v-tabs>
 
-    <v-card
-      color="basil"
-      flat
-      class="pa-5 d-flex flex-column justify-center align-center"
-    >
-      <h1 class="time">{{ displayMinutes }}:{{ displaySeconds }}</h1>
-      <div class="buttons">
-        <v-btn color="primary" @click="start"
-          ><v-icon left small>mdi-play-circle-outline</v-icon>Start</v-btn
+        <v-card
+            color="basil"
+            flat
+            class="pa-5 d-flex flex-column justify-center align-center"
         >
-        <v-btn color="error" @click="stop">
-          <v-icon left small>mdi-stop-circle-outline</v-icon>
-          Stop</v-btn
-        >
-        <v-btn @click="reset" :disabled="isRunning">
-          <v-icon left small>mdi-restart</v-icon>
-          Reset</v-btn
-        >
-      </div>
+            <h1 class="time">{{ displayMinutes }}:{{ displaySeconds }}</h1>
+            <div class="buttons">
+                <v-btn color="primary" @click="start"
+                    ><v-icon left small>mdi-play-circle-outline</v-icon
+                    >Start</v-btn
+                >
+                <v-btn color="error" @click="stop">
+                    <v-icon left small>mdi-stop-circle-outline</v-icon>
+                    Stop</v-btn
+                >
+                <v-btn @click="reset" :disabled="isRunning">
+                    <v-icon left small>mdi-restart</v-icon>
+                    Reset</v-btn
+                >
+            </div>
+        </v-card>
+        <Settings :showSettings="showSettings"></Settings>
     </v-card>
-  </v-card>
 </template>
 
 <script>
+import Settings from "./Settings.vue";
 export default {
-  data() {
-    return {
-      isRunning: false,
-      display: { minutes: "00", seconds: "00" },
-      totalSeconds: 25 * 60,
-      currentTimer: 0,
-      timers: [
-        { name: "Pomodoro", minutes: 25 },
-        { name: "Short Break", minutes: 5 },
-        { name: "Long Break", minutes: 10 },
-      ],
+    components: {
+        Settings
+    },
+    data() {
+        return {
+            isRunning: false,
+            display: { minutes: "00", seconds: "00" },
+            totalSeconds: 25 * 60,
+            currentTimer: 0,
+            timers: [
+                { name: "Pomodoro", minutes: 25 },
+                { name: "Short Break", minutes: 5 },
+                { name: "Long Break", minutes: 10 }
+            ],
+            showSettings: false
+        };
+    },
+    computed: {
+        displayMinutes() {
+            return Math.floor(this.totalSeconds / 60);
+        },
+        displaySeconds() {
+            const seconds = this.totalSeconds % 60;
+            return this.formatTime(seconds);
+        }
+    },
+    methods: {
+        formatTime(time) {
+            if (time < 10) {
+                return "0" + time;
+            }
+            return time.toString();
+        },
+        start() {
+            if (!this.isRunning) {
+                this.timerInstance = setInterval(() => {
+                    if (this.totalSeconds <= 0) {
+                        this.stop();
+                        return;
+                    }
+                    this.totalSeconds -= 1;
+                }, 1000);
+            }
+            this.isRunning = true;
+        },
+        stop() {
+            this.isRunning = false;
+            clearInterval(this.timerInstance);
+        },
+        reset() {
+            this.stop();
+            this.totalSeconds = this.timers[this.currentTimer].minutes * 60;
+        },
+        changeCurrentTimer(num) {
+            this.currentTimer = num;
+            // TODO: Make this toggleable
+            this.reset();
+        }
     }
-  },
-  computed: {
-    displayMinutes() {
-      return Math.floor(this.totalSeconds / 60)
-    },
-    displaySeconds() {
-      const seconds = this.totalSeconds % 60
-      return this.formatTime(seconds)
-    },
-  },
-  methods: {
-    formatTime(time) {
-      if (time < 10) {
-        return "0" + time
-      }
-      return time.toString()
-    },
-    start() {
-      if (!this.isRunning) {
-        this.timerInstance = setInterval(() => {
-          if (this.totalSeconds <= 0) {
-            this.stop()
-            return
-          }
-          this.totalSeconds -= 1
-        }, 1000)
-      }
-      this.isRunning = true
-    },
-    stop() {
-      this.isRunning = false
-      clearInterval(this.timerInstance)
-    },
-    reset() {
-      this.stop()
-      this.totalSeconds = this.timers[this.currentTimer].minutes * 60
-    },
-    changeCurrentTimer(num) {
-      this.currentTimer = num
-      // TODO: Make this toggleable
-      this.reset()
-    },
-  },
-}
+};
 </script>
 
 <style language="sass" scoped>
 .v-card {
-  width: 600px;
+    width: 600px;
 }
 .v-btn {
-  margin: 0 2px;
+    margin: 0 2px;
 }
 .time {
-  font-size: 80px;
-  font-weight: 400;
-  align: center;
+    font-size: 80px;
+    font-weight: 400;
+    align: center;
 }
 </style>
